a3fc54cdf597013dd6089b8cdee1a2a6a31d8c1b
piryakina <62849755+piryakina@users.noreply.github.com>
2020-03-29 21:27:58 +0300

    Update alg.cpp

--- alg.cpp
+++ alg.cpp
@@ -1,28 +1,29 @@
 int cbinsearch(int *arr, int size, int value)
 {
-	int k = 0, s = size, middle = 0, a = 0;
-	while (k < s){
-		middle = (k + s - 1) / 2;
-		if (arr[middle] > value)
-			s = middle - 1;
-		else if (arr[middle] < value)
-			k = middle + 1;
-		else if (arr[middle] == value)
-			a++;
-		break;
-	}
-	if (a == 1)
+int l = 0, s = size, midle = 0, a = 0;
+while (l < s){
+	cout « "work" « endl;
+	midle = (l + s - 1) / 2;
+	if (arr[midle] > value)
+		s = midle - 1;
+	else if (arr[midle] < value)
+		l = midle + 1;
+	else if (arr[midle] == value)
+		a++;
+	break;
+}
+if (a == 1)
+{
+	for (int i = 0; i < midle; i++)
 	{
-		for (int i = 0; i < middle; i++)
-		{
-			if (arr[i] == value)
-				a++;
-		}
-		for (int i = s - 1; i > middle; i--)
-		{
-			if (arr[i] == value)
-				a++;
-		}
+		if (arr[i] == value)
+			a++;
 	}
-	return a;
+	for (int i = s - 1; i > midle; i--)
+	{
+		if (arr[i] == value)
+			a++;
+	}
+}
+return a;
 }
