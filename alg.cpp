506fa6f348d984c4a5eb06f65c7967bfb17ba821
piryakina <62849755+piryakina@users.noreply.github.com>
2020-03-29 21:36:15 +0300

    Create alg.cpp

---
+++ alg.cpp
@@ -0,0 +1,22 @@
+int cbinsearch(int* arr, int size, int value)
+{
+	int i = 0;
+	int j = size - 1;
+	int count = 0,index=0;
+	
+	while (i <= j) {
+		int mid = (i + j) / 2;
+		if (arr[mid] == value) 
+			index = mid;
+	    if (arr[mid] > value) 
+			j = mid - 1;
+		else
+			i = mid + 1;
+	}
+	while (arr[index] == value){
+		count++;
+		index--;
+	}
+
+	return count;
+}
