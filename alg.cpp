445288005eee5139323983798784a544c2593ee5
piryakina <62849755+piryakina@users.noreply.github.com>
2020-03-30 11:31:47 +0300

    Update alg.cpp

--- alg.cpp
+++ alg.cpp
@@ -2,29 +2,27 @@ int cbinsearch(int* arr, int size, int value)
 {
 	int i = 0;
 	int j = size - 1;
-	int count = 0;
-	int s = 0;
-	
+	int count = 0, s = 0;
+
 	while (i <= j) {
 		int mid = i + (j - i) / 2;
 		if (arr[mid] == value) {
 			count++;
 			s = 1;
-			while(arr[mid + k] == value){
+			while (arr[mid + s] == value) {
 				count++;
 				s++;
 			}
 			s = 1;
-			while(arr[mid - k] == value){
+			while (arr[mid - s] == value) {
 				count++;
 				s++;
 			}
 			return count;
 		}
-		if(arr[mid] < value)
+		if (arr[mid] < value)
 			i = mid + 1;
 		else j = mid - 1;
 	}
 	return count;
 }
-
